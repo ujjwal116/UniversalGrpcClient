9c84e6009efae53dc7ce6f61a710b4a03aba6950
ujjwal <ujjwalpandey116@gmail.com>
2020-07-01 23:47:26 +0530

    Building file descriptors

--- src/main/java/com/ugc/CommandExecuter.java
+++ src/main/java/com/ugc/CommandExecuter.java
@@ -15,9 +15,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.github.os72.protocjar.Protoc;
+import com.google.common.collect.ImmutableList;
+import com.google.protobuf.DescriptorProtos.FileDescriptorProto;
 import com.google.protobuf.DescriptorProtos.FileDescriptorSet;
 import com.google.protobuf.DescriptorProtos.MethodDescriptorProto;
 import com.google.protobuf.DescriptorProtos.ServiceDescriptorProto;
+import com.google.protobuf.Descriptors.DescriptorValidationException;
+import com.google.protobuf.Descriptors.FileDescriptor;
 
 import io.grpc.BindableService;
 import lombok.extern.slf4j.Slf4j;
@@ -25,7 +29,7 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @Slf4j
 public class CommandExecuter implements CommandLineRunner {
-	String protoFileRootDirectory = "/home/kramp/Documents/workspace/kramphub-apis/src/main/proto";
+	String protoFileRootDirectory = "D://workspace/kramphub-apis/src/main/proto";
 	String protoFile = "/v1/postino.proto";
 	String protoBufDir = "src/main/resources/protobuf-descriptors/";
 	String pbFile = "descriptor.pb";
@@ -65,11 +69,56 @@ public class CommandExecuter implements CommandLineRunner {
 
 			log.info(serviceToMethodListMap.toString());
 
-			io.grpc.MethodDescriptor.extractFullServiceName("");
+			// Channel channel = ManagedChannelBuilder.forAddress("",
+			// 8080).useTransportSecurity().build();
+			// channel.newCall(mdList.get(0).getDescriptor(). CallOptions.DEFAULT);
 
+			Map<String, FileDescriptorProto> protofileToDescriptorProtoMap = set.getFileList().stream()
+					.collect(Collectors.toMap(protoFile -> protoFile.getName(), protoFile -> protoFile));
+
+			Map<String, FileDescriptor> protoFileToDescriptorMap = new HashMap<>();
+			set.getFileList().forEach(
+					fdp -> resolveFileDescriptor(fdp, protoFileToDescriptorMap, protofileToDescriptorProtoMap));
+
+			System.out.println(protoFileToDescriptorMap);
 		} catch (IOException | InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+
+	private FileDescriptorProto findFileDescriptorProto(String protoName, List<FileDescriptorProto> fileList) {
+		return fileList.stream().filter(fileProto -> fileProto.getName().equals(protoName)).collect(Collectors.toList())
+				.get(0);
+	}
+
+	private void resolveFileDescriptor(FileDescriptorProto fdp, Map<String, FileDescriptor> protofileToDescriptorMap,
+			Map<String, FileDescriptorProto> protofileToDescriptorProtoMap) {
+		try {
+			if (fdp.getDependencyCount() == 0) {
+				protofileToDescriptorMap.put(fdp.getName(), FileDescriptor.buildFrom(fdp, new FileDescriptor[0]));
+				return;
+			}
+			if (protofileToDescriptorMap.containsKey(fdp.getName())) {
+				return;
+			}
+
+			for (String fileName : fdp.getDependencyList()) {
+				if (!protofileToDescriptorMap.containsKey(fileName)) {
+					resolveFileDescriptor(protofileToDescriptorProtoMap.get(fileName), protofileToDescriptorMap,
+							protofileToDescriptorProtoMap);
+				}
+			}
+
+			ImmutableList.Builder<FileDescriptor> prtoDependencyList = ImmutableList.builder();
+			List<FileDescriptor> l = fdp.getDependencyList().stream().map(protofileToDescriptorMap::get)
+					.collect(Collectors.toList());
+
+			protofileToDescriptorMap.put(fdp.getName(),
+					FileDescriptor.buildFrom(fdp, l.toArray(new FileDescriptor[fdp.getDependencyCount()])));
+			return;
+		} catch (DescriptorValidationException e) {
+			log.error("Error while creating protoDescriptor for {} and exception is", fdp.getName(), e);
 		}
 	}
 
