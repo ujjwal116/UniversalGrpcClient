ef661136ad834a1d28ed4122c5b44190fcc03d1d
ujjwal <ujjwal.paandey@kfamp.com>
2020-07-03 00:35:48 +0530

    Build client

--- src/main/java/com/ugc/CommandExecuter.java
+++ src/main/java/com/ugc/CommandExecuter.java
@@ -8,11 +8,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.reflections.Reflections;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 
 import com.github.os72.protocjar.Protoc;
 import com.google.common.collect.ImmutableList;
@@ -22,8 +24,18 @@ import com.google.protobuf.DescriptorProtos.MethodDescriptorProto;
 import com.google.protobuf.DescriptorProtos.ServiceDescriptorProto;
 import com.google.protobuf.Descriptors.DescriptorValidationException;
 import com.google.protobuf.Descriptors.FileDescriptor;
+import com.google.protobuf.Descriptors.MethodDescriptor;
+import com.google.protobuf.DynamicMessage;
+import com.google.protobuf.DynamicMessage.Builder;
+import com.google.protobuf.TypeRegistry;
+import com.google.protobuf.util.JsonFormat;
 
 import io.grpc.BindableService;
+import io.grpc.CallOptions;
+import io.grpc.Channel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.MethodDescriptor.MethodType;
+import io.grpc.stub.ClientCalls;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
@@ -33,6 +45,8 @@ public class CommandExecuter implements CommandLineRunner {
 	String protoFile = "/v1/postino.proto";
 	String protoBufDir = "src/main/resources/protobuf-descriptors/";
 	String pbFile = "descriptor.pb";
+	private String requestBody = "src/main/resources/requests/UpsertPreferencesByEventRequest.json";
+	List<String> fullMethodNames = new ArrayList<>();
 
 	public void buildClient() {
 		Reflections reflections = new Reflections("com.kramphub");
@@ -47,6 +61,7 @@ public class CommandExecuter implements CommandLineRunner {
 	private void listServices() {
 		try {
 			List<String> files = List.of("/v1/postino.proto", "/v1/bouncer.proto");
+			fullMethodNames.add("postino.OrganizationPreferencesService.UpsertPreferencesByEvent");
 
 			Path descriptorPath = Path.of(new File(protoBufDir + pbFile).getAbsolutePath());
 			List<String> protocArgs = new ArrayList<String>();
@@ -68,10 +83,6 @@ public class CommandExecuter implements CommandLineRunner {
 			}));
 
 			log.info(serviceToMethodListMap.toString());
-			// zzxcv
-			// Channel channel = ManagedChannelBuilder.forAddress("",
-			// 8080).useTransportSecurity().build();
-			// channel.newCall(mdList.get(0).getDescriptor(). CallOptions.DEFAULT);
 
 			Map<String, FileDescriptorProto> protofileToDescriptorProtoMap = set.getFileList().stream()
 					.collect(Collectors.toMap(protoFile -> protoFile.getName(), protoFile -> protoFile));
@@ -80,6 +91,42 @@ public class CommandExecuter implements CommandLineRunner {
 			set.getFileList().forEach(
 					fdp -> resolveFileDescriptor(fdp, protoFileToDescriptorMap, protofileToDescriptorProtoMap));
 
+			Map<String, MethodDescriptor> fullNameToMethodDescriptorMap = fetchMethodDescriptorByMethodName(
+					fullMethodNames.stream().collect(Collectors.toSet()), protoFileToDescriptorMap);
+
+			TypeRegistry.Builder typeRegBuilder = TypeRegistry.newBuilder();
+			protoFileToDescriptorMap.forEach((k, v) -> typeRegBuilder.add(v.getMessageTypes()));
+
+			Builder messageBuilder = DynamicMessage
+					.newBuilder(fullNameToMethodDescriptorMap.get(fullMethodNames.get(0)).getInputType());
+
+			JsonFormat.parser().usingTypeRegistry(typeRegBuilder.build())
+					.merge(new String(Files.readAllBytes(ResourceUtils.getFile(requestBody).toPath())), messageBuilder);
+			messageBuilder.build();
+
+			Channel channel = ManagedChannelBuilder.forAddress("localhost", 8080).useTransportSecurity().build();
+
+			io.grpc.MethodDescriptor.Marshaller<DynamicMessage> requestMarshaller = new DynamicMessageMarshaller(
+					fullNameToMethodDescriptorMap.get(fullMethodNames.get(0)).getInputType());
+
+			io.grpc.MethodDescriptor.Marshaller<DynamicMessage> responseMarshaller = new DynamicMessageMarshaller(
+					fullNameToMethodDescriptorMap.get(fullMethodNames.get(0)).getInputType());
+
+			io.grpc.MethodDescriptor<DynamicMessage, DynamicMessage> gcpMethodDescriptor = io.grpc.MethodDescriptor
+					.<DynamicMessage, DynamicMessage>newBuilder().setRequestMarshaller(requestMarshaller)
+					.setResponseMarshaller(responseMarshaller).setFullMethodName(fullMethodNames.get(0))
+					.setType(MethodType.UNARY).build();
+
+			// CommandExecuter
+
+			// (type, fullMethodName, requestMarshaller, responseMarshaller);
+			ClientCalls.asyncUnaryCall(channel.newCall(gcpMethodDescriptor, CallOptions.DEFAULT),
+					messageBuilder.build(), new ResponseObserver());
+
+			/*
+			 * ClientCall<DynamicMessage, DynamicMessage> caller =
+			 * channel.newCall(gcpMethodDescriptor, CallOptions.DEFAULT).;
+			 */
 			System.out.println(protoFileToDescriptorMap);
 		} catch (IOException | InterruptedException e) {
 			// TODO Auto-generated catch block
@@ -87,9 +134,16 @@ public class CommandExecuter implements CommandLineRunner {
 		}
 	}
 
-	private FileDescriptorProto findFileDescriptorProto(String protoName, List<FileDescriptorProto> fileList) {
-		return fileList.stream().filter(fileProto -> fileProto.getName().equals(protoName)).collect(Collectors.toList())
-				.get(0);
+	private Map<String, MethodDescriptor> fetchMethodDescriptorByMethodName(Set<String> setOfFullNames,
+			Map<String, FileDescriptor> protoFileToDescriptorMap) {
+
+		List<MethodDescriptor> mdList = new ArrayList<>();
+		protoFileToDescriptorMap.forEach((k, v) -> v.getServices().forEach(sd -> {
+			mdList.addAll(sd.getMethods());
+		}));
+
+		return mdList.stream().filter(md -> setOfFullNames.contains(md.getFullName()))
+				.collect(Collectors.toMap(md -> md.getFullName(), md -> md));
 	}
 
 	private void resolveFileDescriptor(FileDescriptorProto fdp, Map<String, FileDescriptor> protofileToDescriptorMap,
---
+++ src/main/java/com/ugc/Method.java
@@ -0,0 +1,9 @@
+package com.ugc;
+
+import com.google.protobuf.Descriptors.MethodDescriptor;
+
+public class Method {
+	private String fullName;
+	private MethodDescriptor methodDescriptor;
+
+}
